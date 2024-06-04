aeed262e5521ee92eee52dbf71e848817c5b2d2d
Matthew McMunigle <mcfunigle@Matthews-MacBook-Pro.local>
2024-06-04 11:12:22 -0700

    added loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,22 +24,28 @@ export interface Game {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setErorr] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", {signal: controller.signal})
-      .then((resp) => setGames(resp.data.results))
+      .then((resp) => {
+        setGames(resp.data.results);
+        setLoading(false);
+  })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setErorr(err.message)
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 }
 
 export default useGames;
