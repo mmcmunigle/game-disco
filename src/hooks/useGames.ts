7087b56f80ff3dcb48ebb3d8debba1fd66d40918
Matthew McMunigle <mcfunigle@Matthews-MacBook-Pro.local>
2024-06-10 16:06:52 -0700

    implement infinite scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,6 @@
-import APIClient, { FetchResponse } from "../services/api-client";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
 
 const apiClient = new APIClient<Game>('/games');
@@ -28,7 +28,8 @@ const useGames = (gameQuery: GameQuery) => {
     }),
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
-    }
+    },
+    staleTime: 24 * 60 * 60 * 1000  // 24 hours
   })
 }
 
