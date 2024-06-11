eda688a6b1ca154f9c78a341f6e328120d300bd7
Matthew McMunigle <mcfunigle@Matthews-MacBook-Pro.local>
2024-06-10 18:09:03 -0700

    refactor - use time calculations

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import platforms from '../data/platforms';
-import APIClient, { FetchResponse } from '../services/api-client';
+import APIClient from '../services/api-client';
+import ms from 'ms';
 
 const apiClient = new APIClient<Platform>('/platforms/lists/parents');
 
@@ -13,7 +14,7 @@ const usePlatforms = () => {
     return useQuery({
         queryKey: ['platforms'],
         queryFn: apiClient.getAll,
-        staleTime: 24 * 60 * 60 * 1000, //24h,
+        staleTime: ms('24h'),
         initialData: { count: platforms.length, results: platforms, next: null}
       });
 }
